4201ab5c1b7c3b1bd52ab2628acd2a41c21492f2
Florian Reichl <freichl@google.com>
2026-06-02 02:15:57 -0700

    Decouple CPU PJRT JIT compilation.
    PiperOrigin-RevId: 925211991

--- third_party/xla/xla/pjrt/cpu/cpu_client.h
+++ third_party/xla/xla/pjrt/cpu/cpu_client.h
@@ -83,6 +83,20 @@ namespace xla {
 
 class PjRtCpuExecutable;
 
+// Client-less CPU JIT compilation for XlaComputation.
+absl::StatusOr<std::unique_ptr<PjRtCpuExecutable>> CompileCpuExecutable(
+    const XlaComputation& computation, CompileOptions options,
+    const CpuTopologyDescription& topology,
+    std::function<void(HloModuleConfig&)> customize_hlo_module_config =
+        nullptr);
+
+// Client-less CPU JIT compilation for MLIR Module.
+absl::StatusOr<std::unique_ptr<PjRtCpuExecutable>> CompileCpuExecutable(
+    MaybeOwningMlirModule module, CompileOptions options,
+    const CpuTopologyDescription& topology,
+    std::function<void(HloModuleConfig&)> customize_hlo_module_config =
+        nullptr);
+
 class PjRtCpuClient final : public CommonPjRtClient {
  public:
   ~PjRtCpuClient() override;
@@ -401,9 +415,11 @@ class PjRtCpuExecutable final : public PjRtExecutable {
       std::unique_ptr<Executable> cpu_executable,
       absl::InlinedVector<BufferAllocation::Index, 4> result_buffer_indices,
       std::unique_ptr<HloModule> unoptimized_hlo_module,
-      const CpuTopologyDescription& topology);
+      CpuTopologyDescription topology);
 
   ~PjRtCpuExecutable() override = default;
+
+  absl::Status SetUpDonation(bool tuple_inputs);
 
   absl::string_view name() const override {
     return cpu_executable_->shared_module()->name();
@@ -454,8 +470,6 @@ class PjRtCpuExecutable final : public PjRtExecutable {
   friend class CpuPjRtRawLoadedExecutable;
   friend class PjRtCpuLoadedExecutable;
 
-  absl::Status SetUpDonation(bool tuple_inputs);
-
   int num_replicas_;
   int num_partitions_;
   bool parameter_is_tupled_arguments_;
@@ -492,7 +506,7 @@ class PjRtCpuExecutable final : public PjRtExecutable {
 
   std::unique_ptr<HloModule> unoptimized_hlo_module_;
 
-  const CpuTopologyDescription* topology_;
+  CpuTopologyDescription topology_;
 };
 
 class PjRtCpuLoadedExecutable final : public CommonPjRtLoadedExecutable {
